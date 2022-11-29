385e1c4b1ab77f587a80230e83939e3e66bb0ad8
andrey-araujo <andrey_m2@hotmail.com>
2022-11-29 17:14:44 -0300

    #aula120: creating the chart widget

---
+++ lib/components/chart.dart
@@ -0,0 +1,33 @@
+import 'package:expenses/models/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  const Chart({Key? key, required this.recentTransactions}) : super(key: key);
+
+  final List<Transaction> recentTransactions;
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      return {
+        'day': 'T',
+        'value': 9.99,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(
+        children: const [],
+      ),
+    );
+  }
+}
