4a19c9040bb97475c4a8ef1389914bed2afb5001
andrey-araujo <andrey_m2@hotmail.com>
2022-11-30 18:09:12 -0300

    #aula130: fixing color theme of chart bar

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -47,6 +47,7 @@ class TransactionList extends StatelessWidget {
                   child: ListTile(
                     leading: CircleAvatar(
                       radius: 30,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
                       child: Padding(
                         padding: const EdgeInsets.all(6.0),
                         child: FittedBox(
