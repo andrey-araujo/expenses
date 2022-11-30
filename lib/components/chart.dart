7d611c6d7f1c4d072f5c3e6ce0b474f021473069
andrey-araujo <andrey_m2@hotmail.com>
2022-11-30 11:35:27 -0300

    #aula123: chart_bar development

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
+import 'package:expenses/components/chart_bar.dart';
 import 'package:expenses/models/transaction.dart';
 
 class Chart extends StatelessWidget {
@@ -41,7 +42,11 @@ class Chart extends StatelessWidget {
       margin: const EdgeInsets.all(20),
       child: Row(
         children: groupedTransactions.map((tr) {
-          return Text('${tr['day']}: ${tr['value']}');
+          return ChartBar(
+            label: tr['day'] as String,
+            value: tr['value'] as double,
+            percentage: 0,
+          );
         }).toList(),
       ),
     );
--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-const ChartBar({ Key? key }) : super(key: key);
+  const ChartBar({
+    Key? key,
+    required this.label,
+    required this.value,
+    required this.percentage,
+  }) : super(key: key);
+
+  final String? label;
+  final double? value;
+  final double? percentage;
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('R\$${value?.toStringAsFixed(2)}'),
+        const SizedBox(height: 5,),
+        const SizedBox(
+          height: 60,
+          width: 10,
+          child: null,
+        ),
+        const SizedBox(height: 5),
+        Text(label!)
+      ]
+    );
   }
 }
